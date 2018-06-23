5c38e4eabf155c9ce5c9dde2dca84f76fd7394de
daisuke karino <kingultima1122@gmail.com>
2018-06-23 17:19:28 +0900

    Finish 5-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,16 @@
 class PostImage < ApplicationRecord
 	belongs_to :user
+
+	has_many :favorites, dependent: :destroy
 	has_many :post_comments, dependent: :destroy
+
 	attachment :image # ここを追加（_idは含めません
+
+    validates :shop_name, presence: true
+    validates :image, presence: true
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+
 end
