93d4d1825acf03ecdbcd01c499efc53bd4fbe35d
daisuke karino <kingultima1122@gmail.com>
2018-06-23 16:00:46 +0900

    Finish 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '529a190628187f9bc16b9d1fc7854532177afaa4c87030e2ded9144d7ae301673168ff824ad35cb26c953fdd2db3d8449c287c8133e7b6d8a3e108e803032f8c'
